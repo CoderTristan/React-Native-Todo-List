9be06fd8dbea6b9df41fe0dc50eead57c84fd79c
CoderTristan <tristan.grahl@outlook.com>
2026-05-25 10:27:12 -0400

    did some convex that kind of broke it

--- Project/src/app/(tabs)/index.tsx
+++ Project/src/app/(tabs)/index.tsx
@@ -1,10 +1,30 @@
 import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import useTheme from "../../../hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 export default function Index() {
+  const todos = useQuery(api.todos.getTodos)
+  const addTodo = useMutation(api.todos.addTodo)
+  const clearAllTodo = useMutation(api.todos.clearAllTodos)
+
+
+  const {toggleDarkMode} = useTheme()
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen12355.</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({
+        text: "walk dog"
+      })}>
+        <Text>New Todo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearAllTodo()}>
+        <Text>clear</Text>
+      </TouchableOpacity>
     </View>
   );
 }
